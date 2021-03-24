61b3ce1658c8326f6fa481236231ba47286c80bf
jazzyk <jkirbyy@icloud.com>
2021-03-24 14:19:56 +0000

    Added basic election functionality where agents vote purely on ideoliogical closeness

--- app/agent.py
+++ app/agent.py
@@ -1,4 +1,19 @@
+from party import Party
 class Agent:
     def __init__(self, id, ideology):
         self.id = id
         self.ideology = ideology
+
+    def vote(self, parties):
+        difference = None
+        choice = None
+        for party in parties:
+            current_difference = abs(self.ideology - party.ideology)
+            if choice is None:
+                choice = party
+                difference = current_difference
+            elif current_difference < difference:
+                difference = current_difference
+                choice = party
+
+        return choice
