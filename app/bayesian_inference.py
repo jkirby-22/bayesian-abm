6fb95e8c91a276dae7ccbd099869abefa5e2fa97
jazzyk <jkirbyy@icloud.com>
2021-04-24 20:31:12 +0100

    Enabled multi step inference with shrinking sample size

--- app/bayesian_inference.py
+++ app/bayesian_inference.py
@@ -1,7 +1,7 @@
 import math
 class BayesianInference:
     def __init__(self):
-        self.population = [1, 1, 1, 1, 2]
+        self.population = [1, 1, 2, 1, 1]
         self.step = 0
         v1_prior = [] #Optimistic cand
         for i in range(0, 6):
@@ -12,11 +12,14 @@ class BayesianInference:
         for i in range(0, 6):
             v2_prior.append(self.skeptic_pmf(i))
         self.v2_prior = v2_prior
+        self.unobserved = self.population
+        self.observed = []
 
         #v3_prior = []
         #for i in range(0, 6):
             #v3_prior.append(self.skeptic_pmf(i))
        #self.v3_prior = v3_prior
+
     def optimist_pmf(self, i):
         population = len(self.population)
         majority = math.floor((population / 2) + 1)
@@ -33,23 +36,40 @@ class BayesianInference:
         else:
             return 0
 
+    def remaining(self, i, candidate):
+        count = 0
+        for vote in self.observed:
+            if vote == candidate:
+                count += 1
+        remaining = i - count
+        if remaining < 0:
+            remaining = 0
+        return remaining
+
     def v1_liklihood(self, y, i):
+        sample = len(self.unobserved)
+        remaining = self.remaining(i, 1)
         if y == 1:
-            return i / 5 #change this!
+            return remaining / sample
         else:
-            return (5 - i) / 5
+            return (sample - remaining) / sample
 
     def v2_liklihood(self, y, i):
+        sample = len(self.unobserved)
+        remaining = self.remaining(i, 2)
         if y == 2:
-            return i / 5 #change this!
+            return remaining / sample
         else:
-            return (5 - i) / 5
+            return (sample - remaining) / sample
 
-    def steps(self):
-        y = self.population[0] #change
-        top = []
-        bottom = 0
-        for index in range (0, 6):
+    def bayesian_step(self):
+        y = self.unobserved[0] #change
+        print('y: ' + str(y))
+        top = [] #liklihood * prior for given x and y
+        bottom = 0 #total liklihood x prior for given y
+
+        for index in range(0, 6):
+            #g(index) * f(y | index)
             value = self.v1_prior[index] * self.v1_liklihood(y, index)
             top.append(value)
             bottom += value
@@ -65,12 +85,20 @@ class BayesianInference:
         for index in range(0, 6):
             self.v2_prior[index] = top[index] / bottom
 
+        self.observed.append(self.unobserved.pop(0))
+
 
 if __name__ == '__main__':
     inf = BayesianInference()
     print('Canditate 1 prior (optimist): ' + str(inf.v1_prior))
     print('Canditate 2 prior (skeptic): ' + str(inf.v2_prior))
     print(inf)
-    inf.steps()
+    inf.bayesian_step()
+    print('Candidate 1 posterior: ' + str(inf.v1_prior))
+    print('Candidate 2 posterior: ' + str(inf.v2_prior))
+    inf.bayesian_step()
+    print('Candidate 1 posterior: ' + str(inf.v1_prior))
+    print('Candidate 2 posterior: ' + str(inf.v2_prior))
+    inf.bayesian_step()
     print('Candidate 1 posterior: ' + str(inf.v1_prior))
     print('Candidate 2 posterior: ' + str(inf.v2_prior))
